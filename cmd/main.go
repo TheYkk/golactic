a73ba00bce13b4a4d83f575e6e4b90314aa6abf4
Kaan Karakaya <yusufkaan142@gmail.com>
2021-05-31 19:12:06 +0300

    feat: Added packages to go mod Signed-off-by: Kaan Karakaya <yusufkaan142@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,63 @@
 package main
 
-func main()  {
-	println("sa")
+import (
+	"flag"
+	"github.com/rs/zerolog/log"
+	"github.com/theykk/golactic/pkg/config"
+	"github.com/theykk/golactic/pkg/logger"
+	"github.com/theykk/golactic/pkg/metric"
+	"github.com/theykk/golactic/pkg/tracing"
+	"net/http"
+	"os"
+)
+
+var (
+	Version     = "Dev"
+	ServiceName = "ticker"
+)
+
+var printVersion = flag.Bool("v", false, "Print version")
+var help = flag.Bool("help", false, "Get Help")
+
+func init() {
+	flag.Usage = func() {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	flag.Parse()
+
+	if *help {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	if *printVersion {
+		println(Version)
+		os.Exit(0)
+	}
+
+}
+
+func main() {
+	// Init config
+	configuration := config.InitConfig()
+	configuration.Service.Name = ServiceName
+	configuration.Service.Version = Version
+
+	// Init Logger
+	logger.InitLogger(&configuration)
+
+	//Init Tracing
+	tracing.InitTracer(&configuration)
+
+	// Init Metric
+	metric.InitMetric()
+	// Init GRPC
+
+	// Init HTTP layer for metrics and health check
+	if err := http.ListenAndServe(":"+configuration.Http.Port, nil); err != nil {
+		log.Fatal().Err(err)
+	}
+	// Gracefull shutdown
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,20 @@
 module github.com/theykk/golactic
 
 go 1.16
+
+require (
+	github.com/casbin/casbin/v2 v2.1.2
+	github.com/golang/protobuf v1.5.2
+	github.com/kr/text v0.2.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/prometheus/client_golang v1.10.0
+	github.com/rs/zerolog v1.22.0
+	github.com/sethvargo/go-envconfig v0.3.5
+	go.opentelemetry.io/otel v0.20.0
+	go.opentelemetry.io/otel/exporters/trace/jaeger v0.20.0
+	go.opentelemetry.io/otel/sdk v0.20.0
+	go.opentelemetry.io/otel/trace v0.20.0
+	google.golang.org/grpc v1.38.0
+	google.golang.org/protobuf v1.26.0
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+)
